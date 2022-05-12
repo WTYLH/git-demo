56a6590b6d5df2f15439341b3110fcb37e3a0053
WTY <1248433404@qq.com>
2022-05-12 23:46:45 +0800

    second commit

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,12 +1,6 @@
 #include<stdio.h>
 int main()
 {
-<<<<<<< HEAD
     printf("hello world hello");
     return 0;
-=======
-    int a;
-    printf("hell world");
-    return 0;//666
->>>>>>> 5496d4c7ca5d7404ca0a684e338c1fc627e766a0
 }
